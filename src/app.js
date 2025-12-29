ecde2c7c8c516387d2d580a0f4cea47857bf1909
Prakhar <prakhar.pb01@gmail.com>
2025-12-29 22:37:54 +0530

    add sample error handlers

--- src/app.js
+++ src/app.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const app = express();
 
-const { adminAuth } = require('./middlewares/auth');
-const { userAuth } = require('./middlewares/auth');
-app.use("/admin", adminAuth);
+// best practice to have error handling middleware for each route
+app.get("/getUserData", (req, res) => {
+    //logic of db call and get user data
+    try {
+        throw new error("wrbtrtbklm");
+        res.send("User Route accessed by user");
+    } catch (err) {
+        res.status(500).send("Something went wrong!");
+    }
 
-app.get("/admin/getAllUsers", (req, res) => {
-    res.send("Here is all the users you requested");
 });
-
-app.get("/admin/deleteAllUsers", (req, res) => {
-    res.send("Deleted all users");
-});
-app.get("/user/login", (req, res) => {
-    res.send("User logged in");
-});
-app.get("/user/data", userAuth, (req, res) => {
-    res.send("User route accessed");
+// wildcard error handling middleware and ye last me hota hai
+app.use("/", (err, req, res, next) => {
+    if (err) {
+        //log your error 
+        res.status(500).send("Something went wrong!");
+    }
 })
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
